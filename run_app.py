ab0480795b65b24669a40c75579152afd12bc61e
thienphankh28-art <thienphankh28@gmail.com>
2026-08-22 21:08:17 +0700

    Update GITHUB_CODE_BASE_URL for correct path

--- run_app.py
+++ run_app.py
@@ -11,7 +11,7 @@ import requests
 COPYRIGHT_EMAIL = "thienphankh28@gmail.com"
 
 GITHUB_VERSION_URL = "https://raw.githubusercontent.com/thienphankh28-art/tdm-aminoglycosid-win/refs/heads/main/version.json"
-GITHUB_CODE_BASE_URL = "https://raw.githubusercontent.com/thienphankh28-art/tdm-aminoglycosid-win/"
+GITHUB_CODE_BASE_URL = "https://raw.githubusercontent.com/thienphankh28-art/tdm-aminoglycosid-win/tree/main/"
 
 FILES_TO_UPDATE = ["app.py", "database.py", "pk_calculations.py", "version.json"]
 
